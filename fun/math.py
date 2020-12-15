a48063b5d24f4769d922a418ae9331acb1ad6aba
Dwarakanath Soundararajan <dwarakanathsoundararajan@Dwarakanaths-MacBook-Pro.local>
2020-12-15 21:16:22 +0530

    Just some more fun.

---
+++ fun/math.py
@@ -0,0 +1,11 @@
+a= int(input("Please enter value for A="))
+b= int(input("Please enter value for B="))
+
+print(f"The numbers entered are {a} {b}. The results are as follows")
+print("Addition is",a+b)
+print("Subtraction is",a-b)
+print("Division is",a/b)
+print("Multiplication is",a*b)
+print("Integer Division is",a//b)
+print("Remainder is", a%b)
+print("Exponent is", a**b)
