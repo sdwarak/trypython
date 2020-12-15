a8e4d0551ef09a065df7808fb4590246e887ea02
Dwarakanath Soundararajan <dwarakanathsoundararajan@Dwarakanaths-MacBook-Pro.local>
2020-12-15 21:57:18 +0530

    Sign to fun.

---
+++ fun/sign.py
@@ -0,0 +1,8 @@
+a= int(input("Please enter value for A="))
+
+if a > 0:
+    print("The number is positive")
+elif a == 0:
+    print("The number is zero")
+else:
+    print("The number is negative")
