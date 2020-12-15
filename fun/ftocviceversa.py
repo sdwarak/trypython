66772647e6549b20ff04bd9e7bff51f410f00038
Dwarakanath Soundararajan <dwarakanathsoundararajan@Dwarakanaths-MacBook-Pro.local>
2020-12-15 22:03:26 +0530

    Add F to C conversion to fun.

---
+++ fun/ftocviceversa.py
@@ -0,0 +1,12 @@
+tempf = int(input("Enter temperature in F:\n"))
+
+tempc = (tempf -32)/1.8
+
+print(f"Temprature in C {tempc}")
+
+
+tempca = int(input("Enter temperature in C:\n"))
+
+tempfa = (tempca *1.8+32)
+
+print(f"Temprature in F {tempfa}")
